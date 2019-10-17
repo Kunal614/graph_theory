07d906c42afa8995d3369fbf386f2b880f68f326
kunal kumar barman <kumar96kunal@gmail.com>
2019-10-17 08:30:11 +0530

    Header file of lagorithms

---
+++ header_file/disjoint.h
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class union_set{
+    private:
+        vector<int> rank;
+        vector<int> p;
+        vector<int>cycle;
+    public:
+       int n;
+       union_set(int n){
+       rank.assign(n,0);
+        p.assign(n,0); 
+        cycle.assign(n,0);
+       }
+    
+      int make_set(int n)
+      {
+             
+         
+          for(int i=0;i<n;i++)
+          {
+             p[i]=i;
+             //p.push_back(i);
+            // cout<<p[i]<<" ";
+          }
+
+      }
+      bool isSame_set(int l , int r)
+      {
+            if(find_set(l)==find_set(r))
+                return true;
+            else
+            {
+                  return false;
+            }
+                
+      }
+      int find_set(int k)
+      {
+         // cout<<endl<<p[k]<<endl;
+          //cout<<k<<" "<<p[k]<<endl;
+          if(k==p[k])
+              return k;
+           else
+           {
+             p[k]= find_set(p[k]);
+           }
+              
+      }
+    void Union_set(int l , int r)
+    {
+           if(!isSame_set(l,r))
+           {
+               int x=find_set(l),y=find_set(r);
+               if(rank[x]>rank[y])
+                      p[y]=x;
+                else
+                {
+                    p[x]=y;
+                }
+                if(rank[x]==rank[y])
+                     rank[y]++;
+                      
+           }
+    }
+
+    bool IsCyclic(int n)
+    {
+    bool flag=1;
+        for(int i=0;i<n;i++) 
+             {
+                cycle[i]=find_set(i);
+             }
+        for(int i=0;i<n-1;i++)
+        {
+            if(cycle[i]==cycle[i+1])
+                 flag=0;
+            else
+            {
+                 return false;
+            }
+                 
+        } 
+        if(flag==0)
+            return true;      
+    }
+
+};
